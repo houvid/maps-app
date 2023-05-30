798eb71815f7f0dd23f0af702664be8ea10df438
David Gómez <48219567+houvid@users.noreply.github.com>
2023-05-29 21:32:27 -0500

    Popup, modal dinamico con solucion de estilos

--- src/components/IconLocation.js
+++ src/components/IconLocation.js
@@ -8,3 +8,12 @@ export const iconLocation = new Icon({
   iconSize: [35, 35],
   className: 'leaflet-venue-icon'
 })
+export const iconMarkerGreen = new Icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/7946/7946073.png',
+  iconAnchor: null,
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: [35, 35],
+  className: 'leaflet-venue-icon'
+})
--- src/components/MapLeaflet/MapViewLeaf.js
+++ src/components/MapLeaflet/MapViewLeaf.js
@@ -1,20 +1,33 @@
-import { React, useContext, useEffect, useLayoutEffect } from 'react'
+import { React, useContext, useEffect, useState } from 'react'
 import { PlacesContext, ModalContext } from '../../context'
 import { Markers } from './Markers'
-import { iconLocation } from '../IconLocation'
+import { iconLocation, iconMarkerGreen } from '../IconLocation'
 import 'leaflet/dist/leaflet.css'
+import { Modal, Button } from 'react-bootstrap'
 
-import { MapContainer, Marker, Popup, TileLayer, useMap, LatLngLiteral } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import '../../assets/leaflet.css'
 export const MapViewLeaf = () => {
   const { isLoading, userLocation, places, isLoadingPlaces, SetPlacesInit } = useContext(PlacesContext)
-  const { SetStateModal, SetPlace } = useContext(ModalContext)
+  const { SetStateModal, SetPlace, place } = useContext(ModalContext)
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [toggleState, setToggleState] = useState(0)
   useEffect(() => {
     SetPlacesInit()
   }, [])
   if (isLoading || !userLocation || isLoadingPlaces) {
     // Renderiza un indicador de carga o cualquier otro contenido mientras se obtienen los datos de ubicación
     return <div>Cargando...</div>
+  }
+  const openModal = (place) => {
+    setModalIsOpen(true)
+    SetPlace(place)
+  }
+  const closeModal = () => {
+    setModalIsOpen(false)
+  }
+  const toggleTab = (index) => {
+    setToggleState(index)
   }
   const openDetails = (state, place) => {
     const botonCerrar = document.querySelector('.leaflet-popup-close-button')
@@ -26,22 +39,89 @@ export const MapViewLeaf = () => {
     <MapContainer center={userLocation} zoom={13} scrollWheelZoom>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
       />
       <Markers />
       {places.map((place, index) => (
-        <Marker key={index} position={[place.geometry.coordinates[1], place.geometry.coordinates[0]]} icon={iconLocation}>
-          <Popup>
+        <Marker key={index} position={[place.geometry.coordinates[1], place.geometry.coordinates[0]]} icon={iconMarkerGreen}>
+          <Popup className='custom-popup'>
             <div>
               <img src={place.properties?.urlImagen} alt='img' />
-              <h2>{place.properties.name}</h2>
+              <p>{place.properties.name}</p>
               <p>{place.properties.descripcion}</p>
-              <button className='btn btn-primary' onClick={() => openDetails(true, place)}> Ver mas detalles</button>
+              <button className='btn btn-primary' onClick={() => openModal(place)}> Ver mas detalles</button>
             </div>
           </Popup>
         </Marker>
       ))}
-
+      <section style={styles.modal}>
+        <Modal show={modalIsOpen} onHide={closeModal} className='contenedorPrincipalModal' style={styles.modalContainer} dialogClassName='modal-right'>
+          <div className='modal__close close-modal' title='Close' onClick={closeModal}>
+            <i className='bx bx-x' />
+          </div>
+          <div className='slider'>
+            <img src={place.properties?.urlImagen} alt='' className='modal__img' />
+            <img src={place.properties?.urlImagen} alt='' className='modal__img' />
+            <img src={place.properties?.urlImagen} alt='' className='modal__img' />
+            <img src={place.properties?.urlImagen} alt='' className='modal__img' />
+          </div>
+          <div className='contenedor'>
+            <ul className='ul'>
+              <li className={toggleState === 0 ? 'li activo' : 'li'} onClick={() => toggleTab(0)}>Info</li>
+              <li className={toggleState === 1 ? 'li activo' : 'li'} onClick={() => toggleTab(1)}>Horarios</li>
+              <li className={toggleState === 2 ? 'li activo' : 'li'} onClick={() => toggleTab(2)}>Recomendamos</li>
+            </ul>
+            <div className='subcontenedor'>
+              <div className={toggleState === 0 ? 'bloque activo' : 'bloque'}>
+                <h1 className='modal__title'>{place.properties?.name}</h1>
+                <p className='modal__description'>{place.properties?.descripcion}</p>
+              </div>
+              <div className={toggleState === 1 ? 'bloque activo' : 'bloque'}>
+                <p className='modal__description '>Lunes a viernes: de 9:00 a.m. a 5:00 p.m.
+                  Sábados: de 10:00 a.m. a 6:00 p.m.
+                  Domingos: cerrado
+                  Nota: Los horarios pueden variar durante días festivos y eventos especiales. Por favor, consulte nuestra página web o llámenos para más información
+                </p>
+                .
+              </div>
+              <div className={toggleState === 2 ? 'bloque activo' : 'bloque'}>por ahora nada xd</div>
+            </div>
+          </div>
+          <Modal.Footer>
+            <button className='modal__button-link close-modal' onClick={closeModal}>
+              Close
+            </button>
+          </Modal.Footer>
+        </Modal>
+      </section>
     </MapContainer>
   )
+}
+const styles = {
+  modalContainer: {
+    borderRadius: '1rem 1rem 0 0',
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    buttom: '0',
+    backgroundcolor: 'hsla(var(--hue), 18%, 75%, .8)',
+    width: '100%',
+    display: 'grid',
+    alignitems: 'flex-end',
+    transition: 'all .3s',
+    zindex: 'var(--z-modal)',
+    float: 'right'
+  },
+  closeBtn: {
+    position: 'absolute',
+    top: 0,
+    right: 0
+  },
+  '@media (max-width: 767px)': {
+    modal: {
+      height: '85%',
+      bottom: 0,
+      top: 'auto'
+    }
+  }
 }
