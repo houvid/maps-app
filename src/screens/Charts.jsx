de91132302bc1e912dcec6fb4df3db0609ab44c2
David Gómez <48219567+houvid@users.noreply.github.com>
2023-06-16 21:57:41 -0500

    Cambio de tamaño charts

--- src/screens/Charts.jsx
+++ src/screens/Charts.jsx
@@ -8,14 +8,14 @@ function Charts () {
       <h1 className='text-center'>Métricas</h1>
       <div>
         <p className='m-2 text-center'><b>Cantidad de visitantes </b></p>
-        <div className='bg-light mx-auto px-2 border border-2 border-primary' style={{ width: '450px', height: '230px' }}>
+        <div className='bg-light mx-auto px-2 border border-2 border-primary' style={{ width: '350px', height: '200px' }}>
           <LinesChart />
         </div>
       </div>
       <hr className='mt-3 mb-2' />
       <div>
         <p className='m-2 text-center'><b>Edades: </b> </p>
-        <div className='bg-light mx-auto border border-2 border-primary' style={{ width: '450px', height: '250px' }}>
+        <div className='bg-light mx-auto border border-2 border-primary' style={{ width: '350px', height: '200px' }}>
           <div style={{ width: '100%', height: '100%', padding: '10px 0' }}>
             <Pies />
           </div>
