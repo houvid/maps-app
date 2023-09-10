602af8f45fb1d2d8d31cfb97f9d64028a856c3de
David Gómez <48219567+houvid@users.noreply.github.com>
2023-09-10 17:09:55 -0500

    Se agrega eventos a la zona de administración

---
+++ src/components/Features/ListFeatures.jsx
@@ -0,0 +1,98 @@
+/* eslint-disable no-unused-vars */
+import { NavBar } from '../NavBar'
+import { useContext, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { PlacesProvider, PlacesContext } from '../../context'
+import { ModalContext } from '../../context/modal/ModalContext'
+import { ModalEventos } from '../Modals/modalEventos'
+
+export const ListFeatures = () => {
+  const { isLoading, userLocation, isLoadingPlaces, SetPlacesInit, places } = useContext(PlacesContext)
+  const { SetStateModalEvent, SetPlace } = useContext(ModalContext)
+  useEffect(() => {
+    console.log(isLoading, userLocation, isLoadingPlaces)
+    SetPlacesInit()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  if (isLoading || !userLocation || isLoadingPlaces) {
+    return (
+      <div className='backLoader loading-map d-flex justify-content-center aling-items-center'>
+        <img src='logo-2 Blanco.png' alt='img' style={{ height: '170px', padding: '50px' }} />
+        <span className='loader'> </span>
+      </div>
+    )
+  }
+  const openModal = (place) => {
+    SetPlace(place)
+    SetStateModalEvent(true)
+  }
+  console.log(places)
+  return (
+    <div>
+      <div className='container section'>
+        <h2 className='text-center'>(MODULO EN CONSTRUCCIÓN) ¡Estos son los registros que tenemos actualmente!</h2>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Img</th>
+                <th scope='col'>Nombre</th>
+                <th scope='col'>Categoría</th>
+                <th scope='col'>Img</th>
+                <th scope='col'>Eventos</th>
+              </tr>
+            </thead>
+            <tbody>
+              {places.map((place, index) => (
+                <tr key={index}>
+                  <th scope='row'>{index + 1}</th>
+                  <td>{place.id}</td>
+                  <td>{place.properties.name}</td>
+                  <td>{place.properties.categoria}</td>
+                  <td><img src={place.properties.urlImagen} alt='img' style={{ width: '100px' }} /> </td>
+                  <td>
+                    <button className='modal__button-link close-modal' onClick={() => openModal(place)}>
+                      Ver
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <section style={styles.modal}>
+            <ModalEventos />
+          </section>
+        </div>
+      </div>
+    </div>
+  )
+}
+const styles = {
+  modalContainer: {
+    borderRadius: '1rem 1rem 0 0',
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    buttom: '0',
+    backgroundcolor: 'hsla(var(--hue), 18%, 75%, .8)',
+    width: '100%',
+    display: 'grid',
+    alignitems: 'flex-end',
+    transition: 'all .3s',
+    zindex: 'var(--z-modal)',
+    float: 'right'
+  },
+  closeBtn: {
+    position: 'absolute',
+    top: 0,
+    right: 0
+  },
+  '@media (max-width: 767px)': {
+    modal: {
+      height: '85%',
+      bottom: 0,
+      top: 'auto'
+    }
+  }
+}
--- src/screens/Features.jsx
+++ src/screens/Features.jsx
@@ -1,59 +1,20 @@
 /* eslint-disable no-unused-vars */
 import { useAuth } from '../context/authContext'
 import { NavBar } from '../components/NavBar'
-import { useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { PlacesContext } from '../context'
+import { PlacesProvider, PlacesContext } from '../context'
+import { ModalProvider } from '../context/modal/ModalProvider'
+import { ListFeatures } from '../components/Features/ListFeatures'
 
 export const Features = () => {
-  const { places } = useContext(PlacesContext)
   const auth = useAuth()
-  const navigate = useNavigate()
   const userEmail = auth.user.email
   console.log(userEmail)
-  if (!userEmail) {
-    navigate('/login')
-    return (<div> <span className='loader' /> </div>)
-  }
   return (
-
-    <div><NavBar />
-      <div className='container section'>
-        <h2 className='text-center'>(MODULO EN CONSTRUCCIÓN) ¡Estos son los registros que tenemos actualmente!</h2>
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <table className='table'>
-            <thead>
-              <tr>
-                <th scope='col'>#</th>
-                <th scope='col'>Img</th>
-                <th scope='col'>Nombre</th>
-                <th scope='col'>Latitud</th>
-                <th scope='col'>longitud</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope='row'>1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope='row'>2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope='row'>3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
+    <ModalProvider>
+      <PlacesProvider>
+        <NavBar />
+        <ListFeatures />
+      </PlacesProvider>
+    </ModalProvider>
   )
 }
